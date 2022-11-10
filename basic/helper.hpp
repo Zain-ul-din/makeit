9c3f2c18b44f362cbedc853f21dbcdd7b715b068
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-10 06:38:40 +0500

    added header files

---
+++ basic/helper.hpp
@@ -0,0 +1,13 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+/// @brief adds two number
+/// @param int num1 
+/// @param int num2 
+/// @return sum of two numbers
+int addToNumbers (int num1, int num2)
+{
+    return num1 + num2;
+}
+
+#endif
--- basic/index.cpp
+++ basic/index.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <string>
-#include <vector>
-
+#include "./helper.hpp"
 
 // Driver code
 int main (int argc, char* argv[])
 {
     std::cout << "Hello C-Make using clang++\n";
+    int num1 = 10, num2 = 20;
+    std::cout << "Sum : " << addToNumbers (num1, num2);
     std::cin.get ();
     return 0;
 }
